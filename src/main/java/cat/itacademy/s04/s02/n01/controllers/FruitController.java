834112425b889150b5cf1bf4f89b0b04410ae946
Daniel Caldito Serrano <dcs1990x@gmail.com>
2025-11-14 20:53:21 +0100

    feat: added config class and added Swagger documentation feature

--- src/main/java/cat/itacademy/s04/s02/n01/controllers/FruitController.java
+++ src/main/java/cat/itacademy/s04/s02/n01/controllers/FruitController.java
@@ -4,11 +4,16 @@ import cat.itacademy.s04.s02.n01.model.Fruit;
 import cat.itacademy.s04.s02.n01.model.FruitDTO;
 import cat.itacademy.s04.s02.n01.model.PutFruitCommand;
 import cat.itacademy.s04.s02.n01.services.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
+@Tag(name="Search API", description="Fruit search API")
 public class FruitController {
 
     private final CreateFruitService createFruitService;
@@ -30,26 +35,39 @@ public class FruitController {
     }
 
     @PostMapping("/fruit")
+    @Operation(summary = "Create a fruit")
+    @ApiResponse(responseCode = "201", description = "The fruit was created successfully")
     public ResponseEntity<FruitDTO> postFruit(@RequestBody Fruit fruit){
         return createFruitService.execute(fruit);
     }
 
     @GetMapping("/fruits")
+    @Operation(summary = "Get all fruits")
+    @ApiResponse(responseCode = "200", description = "All fruits are found")
+    @ApiResponse(responseCode = "404", description = "No fruit was found")
     public ResponseEntity<List<FruitDTO>> getFruits(){
         return readFruitService.execute(null);
     }
 
     @GetMapping("/fruit/{id}")
+    @Operation(summary = "Find fruit by ID")
+    @ApiResponse(responseCode = "200", description = "The fruit was found")
+    @ApiResponse(responseCode = "404", description = "The fruit was not found")
     public ResponseEntity<FruitDTO> getFruitById(@PathVariable int id){
         return readFruitByIdService.execute(id);
     }
 
     @PutMapping("/fruit/{id}")
+    @Operation(summary = "Update fruit by ID")
+    @ApiResponse(responseCode = "200", description = "The fruit was updated successfully")
+    @ApiResponse(responseCode = "201", description = "The fruit was created successfully")
     public ResponseEntity<FruitDTO> putFruit(@PathVariable int id, @RequestBody Fruit fruit){
         return updateFruitService.execute(new PutFruitCommand(id, fruit));
     }
 
     @DeleteMapping("/fruit/{id}")
+    @Operation(summary = "Delete fruit by ID")
+    @ApiResponse(responseCode = "200", description = "The fruit was deleted successfully")
     public ResponseEntity<Void> deleteFruit(@PathVariable int id){
         return deleteFruitService.execute(id);
     }
